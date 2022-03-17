beec305498ef1b70c9c99f76817d4e7973b8eb5f
Kid <44045911+kidonng@users.noreply.github.com>
2022-03-17 19:36:57 +0800

    Lint `fork-source-link-same-view`

--- source/features/fork-source-link-same-view.tsx
+++ source/features/fork-source-link-same-view.tsx
@@ -15,7 +15,7 @@ async function getEquivalentURL(): Promise<string> {
 	const forkedRepository = getRepo(getForkedRepo())!;
 	const defaultUrl = '/' + forkedRepository.nameWithOwner;
 
-	if (pageDetect.isConversation() || pageDetect.isRepoRoot()) {
+	if (pageDetect.isIssue() || pageDetect.isPR() || pageDetect.isRepoRoot()) {
 		// We must reset the link because the header is outside the ajaxed area
 		return defaultUrl;
 	}
@@ -37,7 +37,7 @@ async function getEquivalentURL(): Promise<string> {
 
 async function init(): Promise<void> {
 	// The link must always be updated/reset. This pattern ensures that the link is always updated and never fails through some conditions.
-	select<HTMLAnchorElement>(`[data-hovercard-url="/${getForkedRepo()!}/hovercard"]`)!.href = await getEquivalentURL();
+	select(`a[data-hovercard-url="/${getForkedRepo()!}/hovercard"]`)!.href = await getEquivalentURL();
 }
 
 void features.add(import.meta.url, {
